3074164bc0f0d3f205dd8144227405298197e059
alex123bob <alex123bobo@gmail.com>
2024-01-29 16:41:10 +0800

    change script to make it well

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 # This may varies on your system. Check your executable path with `whereis python3`
-nohup /usr/local/bin/python3 ./app.py > ~/ppaiservice.log 2>&1 &
+nohup /usr/local/bin/python3 ~/PP-AI-Service/app.py > ~/ppaiservice.log 2>&1 &
